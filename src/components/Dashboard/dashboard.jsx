a5d6d753965afdbca1963192319a31899b77dca4
peedrou <pedrofsalesvieira@gmail.com>
2022-12-30 16:21:33 +0000

    modified:   src/components/Dashboard/dashboard.jsx

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,13 +1,41 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../../contexts/AuthContext.js";
 import { useNavigate } from "react-router-dom";
+import { firestore } from "../../firebase.js";
 import "./dashboard.scss";
 
 function Dashboard() {
   const [error, setError] = useState("");
+  const [finalAddress, setFinalAddress] = useState("");
   const { currentUser, logout } = useAuth();
   const navigate = useNavigate();
+  const db = firestore;
+
+  useEffect(() => {
+    async function fullAddress() {
+      let street;
+      let city;
+      let country;
+      let postalCode;
+      const userRef = db.collection("users").doc(currentUser.uid);
+      await userRef.get("Street").then((doc) => {
+        street = doc.data()["Street"];
+      });
+      await userRef.get("City").then((doc) => {
+        city = doc.data()["City"];
+      });
+      await userRef.get("Country").then((doc) => {
+        country = doc.data()["Country"];
+      });
+      await userRef.get("PostalCode").then((doc) => {
+        postalCode = doc.data()["PostalCode"];
+      });
+
+      setFinalAddress(`${street}, ${postalCode} ${city}, ${country}`);
+    }
+    fullAddress();
+  }, []);
 
   async function handleLogout() {
     setError("");
@@ -19,9 +47,6 @@ function Dashboard() {
       setError("Failed to Log Out");
     }
   }
-
-  console.log(currentUser);
-
   return (
     <div className="dashboard-main-div">
       <div className="profile-main-wrapper">
@@ -35,7 +60,7 @@ function Dashboard() {
           </h3>
           <a href="/address" className="address-a">
             <h3 className="address-heading">
-              <b>Address:</b>
+              <b>Address:</b> {finalAddress}
             </h3>
           </a>
           <h3 className="orders-heading">
