98df2f9faa5d6ddbbabefb3acda769dc18aa1e21
peedrou <pedrofsalesvieira@gmail.com>
2023-01-18 20:59:28 +0000

    modified:   src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,16 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-console.log(process.env.REACT_APP_APIKEY);
 
-const app = firebase.initializeApp({});
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDERID,
+  appId: process.env.REACT_APP_APPID,
+});
 
 export const auth = app.auth();
 export const firestore = firebase.firestore();
