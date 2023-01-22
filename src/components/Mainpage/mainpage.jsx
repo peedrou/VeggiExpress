b3cd2fa9303ce3f03247abc14f80e911a881ed11
peedrou <pedrofsalesvieira@gmail.com>
2023-01-22 23:28:02 +0000

    modified:   src/components/Mainpage/mainpage.jsx 	modified:   src/components/Mainpage/mainpage.scss 	new file:   src/images/burgermenu.png

--- src/components/Mainpage/mainpage.jsx
+++ src/components/Mainpage/mainpage.jsx
@@ -5,6 +5,7 @@ import veggieicon from "../../images/veggie-icon.png";
 import veggiexpress from "../../images/veggiexpresss.png";
 import bananafruit from "../../images/bananafruit.png";
 import grapesfruit from "../../images/grapesfruit.png";
+import burger from "../../images/burgermenu.png";
 import React from "react";
 import { useState, useEffect } from "react";
 import { useAuth } from "../../contexts/AuthContext";
@@ -15,7 +16,34 @@ function Mainpage() {
   const { currentUser, logout } = useAuth();
   const [logged, setLogged] = useState(false);
   const [orderRedirect, setOrderRedirect] = useState(false);
+  const [burgerMenu, setBurgerMenu] = useState(false);
+  const [pushMenu, setPushMenu] = useState(false);
+  const [transformMenu, setTransformMenu] = useState(0);
   const Navigate = useNavigate();
+
+  useEffect(() => {
+    let cb = function () {
+      const totalWidth = window.innerWidth;
+      if (totalWidth <= 800) {
+        setBurgerMenu(true);
+      } else {
+        setBurgerMenu(false);
+      }
+    };
+    window.addEventListener("resize", cb);
+
+    return () => {
+      window.removeEventListener("resize", cb);
+    };
+  });
+
+  useEffect(() => {
+    if (!pushMenu) {
+      setTransformMenu(-250);
+    } else {
+      setTransformMenu(0);
+    }
+  });
 
   useEffect(() => {
     if (logged) {
@@ -51,45 +79,90 @@ function Mainpage() {
         <div className="image-div">
           <img className="logo" alt="placeholder" src={veggiexpress} />
         </div>
-        {logged == false && (
-          <div className="log-sign-div">
-            {orderRedirect && (
-              <div className="log-first">
-                You must log in or sign up before ordering!
+        {!burgerMenu && (
+          <>
+            {logged == false && (
+              <div className="log-sign-div">
+                {orderRedirect && (
+                  <div className="log-first">
+                    You must log in or sign up before ordering!
+                  </div>
+                )}
+                <a href="/login" className="login-a">
+                  <h2 className="log-in">Log In</h2>
+                  <img
+                    src={grapesfruit}
+                    alt="Image not found"
+                    className="grapesfruit"
+                  />
+                </a>
+                <a href="/signup" className="signup-a">
+                  <h2 className="sign-up">Sign Up</h2>
+                  <img
+                    src={bananafruit}
+                    alt="Image not found"
+                    className="bananafruit"
+                  />
+                </a>
               </div>
             )}
-            <a href="/login" className="login-a">
-              <h2 className="log-in">Log In</h2>
-              <img
-                src={grapesfruit}
-                alt="Image not found"
-                className="grapesfruit"
-              />
-            </a>
-            <a href="/signup" className="signup-a">
-              <h2 className="sign-up">Sign Up</h2>
-              <img
-                src={bananafruit}
-                alt="Image not found"
-                className="bananafruit"
-              />
-            </a>
+            <div className="logged-super-div">
+              {logged && (
+                <div className="logged-in-div">
+                  <a href="/dashboard" className="loggen-in-a">
+                    <h2>My Profile</h2>
+                  </a>
+                </div>
+              )}
+              {logged && (
+                <div className="sign-out-div" onClick={() => handleLogout()}>
+                  <h1 className="sign-out-heading">Sign Out</h1>
+                </div>
+              )}
+            </div>
+          </>
+        )}{" "}
+        {burgerMenu && (
+          <div className="burger-main-div">
+            <div>
+              {!logged && (
+                <div
+                  className="burger-notlogged-div"
+                  style={{ transform: `translateX(${transformMenu}px)` }}
+                >
+                  <a href="/login" className="burger-login-a">
+                    <h4 className="burger-login-heading">Log In</h4>
+                  </a>
+                  <a href="/signup" className="burger-signup-a">
+                    <h4 className="burger-signup-heading">Sign Up</h4>
+                  </a>
+                </div>
+              )}
+              {logged && (
+                <div
+                  className="burger-logged-div"
+                  style={{ transform: `translateX(${transformMenu}px)` }}
+                >
+                  <a href="/dashboard" className="burger-dashboard-a">
+                    <h4 className="burger-dashboard-heading">My Profile</h4>
+                  </a>
+                  <h4
+                    className="burger-signout-heading"
+                    onClick={() => handleLogout()}
+                  >
+                    Sign Out
+                  </h4>
+                </div>
+              )}
+            </div>{" "}
+            <img
+              className="burger-img"
+              style={{ transform: `translateX(${transformMenu}px)` }}
+              onClick={() => setPushMenu(!pushMenu)}
+              src={burger}
+            />
           </div>
         )}
-        <div className="logged-super-div">
-          {logged && (
-            <div className="logged-in-div">
-              <a href="/dashboard" className="loggen-in-a">
-                <h2>My Profile</h2>
-              </a>
-            </div>
-          )}
-          {logged && (
-            <div className="sign-out-div" onClick={() => handleLogout()}>
-              <h1 className="sign-out-heading">Sign Out</h1>
-            </div>
-          )}
-        </div>
       </div>
       <div className="slogan-option-wrapper">
         <img className="person" src={person} />
@@ -133,7 +206,7 @@ function Mainpage() {
           >
             <div className="membership-button-wrapper">
               <button className="membership-button">
-                I want the basket every month!
+                I want a basket every month!
               </button>
               <div className="veggies-wrapper">
                 <img className="veggie-icon2" src={veggieicon} />
