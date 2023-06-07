543a97ac2b4e1ce66ca51b37d413d44e9bc87d24
peedrou <pedrofsalesvieira@gmail.com>
2023-06-07 19:12:19 +0100

    changed address info

--- src/components/Address/address.jsx
+++ src/components/Address/address.jsx
@@ -81,10 +81,10 @@ function Address() {
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">
-              Personal Information
+              Address Information
             </h2>
             <p className="mt-1 text-sm leading-6 text-gray-600">
-              Use a permanent address where you can receive mail.
+              Use a permanent address where you can receive your orders.
             </p>
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
